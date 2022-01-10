ab0b5ac104029b0059223c6ed8edfd007b5229fc
matheusasw <80736717+MatheusFariasRS@users.noreply.github.com>
2022-01-10 19:07:04 -0300

    funções em andamento
    conseguir avançar com o as funções de compra e venda de moedas na casa de cambio

--- casa de cambio/script.js
+++ casa de cambio/script.js
@@ -4,18 +4,30 @@ let libra = 7.70;
 let iene = 0.049;
 let real = 1;
 
-function comprarMoeda (moeda, quantidade) {
-        return(quantidade / moeda * 0.10)
-}
+function venderMoeda (quantidade, moeda) {
+     
+    let vender = quantidade / moeda;
+    let taxa = 0.10;
+    let total = vender * taxa;
 
-function venderMoedas (moeda, quantidade) {
-    return (moeda * quantidade * 0.10);
-}
-
-function trocarMoeda (moeda, quantidade, moeda2) {
-    if (moeda > 0) {
-        continue;
-    } else if (quantidade > 0) {
-        return venderMoedas;
+    if (quantidade / moeda > 0)
+     {
+        console.log("O valor da taxa é: " + Math.floor(vender * taxa) + " Valor a pagar: " + Math.floor(vender + total));
     }
 }
+        
+    
+ function comprarMoeda (quantidade, moeda){
+    
+     let comprar = quantidade / moeda;
+     let taxa = 0.10;
+     let total = comprar * taxa;
+
+     if (quantidade / moeda > 0)
+      {
+         console.log("O valor da taxa é: " + Math.floor(comprar * 0.10) + " Valor a pagar " + Math.floor(comprar - total))
+     }
+
+    }
+ 
+
