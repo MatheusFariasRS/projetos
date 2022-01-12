6fbceb69ba17470b36478eb9952de52d5dcc1b12
matheusasw <80736717+MatheusFariasRS@users.noreply.github.com>
2022-01-12 12:35:56 -0300

    terminada a logica
    fim da logica em js por agr, futura atualização, criar um interface com html+css

--- mercado/script.js
+++ mercado/script.js
@@ -1,20 +1,48 @@
 function estoqueProdutos (nome, peso, preço) {
-    return {nome, peso, preço}
+    return {nome, peso, preço};
 }
 
-let produtos {
-    arroz: estoqueProdutos ("arroz", 1, 4),
+let produtos = {
+    arroz: estoqueProdutos ("Arroz", 1, 4),
     feijao: estoqueProdutos ("feijão", 1, 3),
-    massa: estoqueProdutos ("massa", 1, 2),
-    açucar: estoqueProdutos ("açúcar", 1, 2),
+    massa: estoqueProdutos ("Massa", 1, 2),
+    açucar: estoqueProdutos ("Açúcar", 1, 2),
 }
 
 let mercado = {
-    desconto = 0.40
+    desconto: 0.10,
+    desconto2: 0.0
 }
+
 
 let cliente = {
-    lucro = 0.40
+    lucro: 0.40
 }
 
-mercado.estoque = 
+mercado.comprarEstoque = function (produto, quantidade) {
+    if (quantidade >= 100) {
+        let valorDesconto = produto.preço - (produto.preço * this.desconto);
+    return valorDesconto * quantidade;
+    }
+     else if (quantidade < 100) {
+         let valorDesconto2 = produto.preço -  this.desconto2;
+    return valorDesconto2 * quantidade;
+    }
+}
+
+cliente.venderEstoque = function (produto, quantidade) {
+    let valorlucro = produto.preço * (1 + this.lucro);
+    return valorlucro * quantidade;
+}
+
+mercado.criarTabela = function (produto) {
+    let tabela = [];
+
+    for (let moeda in moedas){
+        tabela.push ({
+            "Produtos": produtos[produto].nome + ' (' + produtos[produto].sigla + ') ',
+            "Valor de estoque": this.comprarEstoque(produtos[produto, 1]),
+            "Valor de venda": this.venderEstoque(produtos[produto, 1])
+        })
+    }
+}
