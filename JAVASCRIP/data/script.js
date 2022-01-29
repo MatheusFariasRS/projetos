4581d9c367a100a53df63686fb7f8cea929b579f
matheusasw <80736717+MatheusFariasRS@users.noreply.github.com>
2022-01-29 14:46:54 -0300

    dfafda

---
+++ JAVASCRIP/data/script.js
@@ -0,0 +1,8 @@
+let d = new Date("10-24-1999");
+
+let dias = ["DOM", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
+
+console.log(d);
+console.log(dias[d.getDay()])
+console.log(d.setDate(20));
+console.log(d);
